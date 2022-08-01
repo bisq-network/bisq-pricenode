e71a95e600ba46152f9f3a724c6d736accab05ba
ghubstan <36207203+ghubstan@users.noreply.github.com>
2022-08-01 11:46:17 -0300

    Fix loop.sh jar name (has a version in jar name)

--- docker/loop.sh
+++ docker/loop.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+
+# Must be run from project directory.
+
+VERSION=$(tr -d '\n' < ./src/main/resources/version.txt)
+echo "bisq-pricenode version = $VERSION"
+JAR="./build/libs/bisq-pricenode-$VERSION.jar"
+echo "bisq-pricenode jar = $JAR"
+
 while true
 do
-echo `date`  "(Re)-starting node"
-java -jar ./build/libs/bisq-pricenode.jar 2 2
-echo `date` "node terminated unexpectedly!!"
+echo `date`  "(Re)-starting bisq-pricenode"
+java -jar $JAR 2 2
+echo `date` "Node terminated unexpectedly!!"
 sleep 3
 done
