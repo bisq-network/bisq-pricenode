a3cd9a198a270e3f4041b5fd1dc49029cf4af85d
Rodrigo Varela <rodvar@gmail.com>
2023-07-13 16:45:53 +1000

    Add support for ARS free market price
     - setup cryptoya api price provider
     - add cryptoya pricenode provider
     - algorithm to decide rate based on fetched tickers
     - add test for new exchange rate provider
     - code review suggestions

---
+++ src/main/java/bisq/price/spot/providers/CryptoYa.java
@@ -0,0 +1,86 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.price.spot.providers;
+
+import bisq.price.spot.ExchangeRate;
+import bisq.price.spot.ExchangeRateProvider;
+import bisq.price.util.cryptoya.CryptoYaMarketData;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.RequestEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * CryptoYa is used only for Argentina Peso (ARS).
+ * Currency controls in the country forces a black market where the currency can be traded freely.
+ * Official and easily available rates cannot be trusted, therefore a specific fetch needs to be done
+ * for these scenarios.
+ * This ExchangeRateProvider provides a real market rate (black or "blue") for ARS/BTC
+ */
+@Component
+class CryptoYa extends ExchangeRateProvider {
+
+    private static final String ARS_BTC_URL = "https://criptoya.com/api/btc/ars/0.1";
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public CryptoYa(Environment env) {
+        super(env, "CRYPTOYA", "cryptoya", Duration.ofMinutes(1));
+    }
+
+    /**
+     *
+     * @return average price buy/sell price averaging different providers suported by cryptoya api
+     * which uses the free market (or blue, or unofficial) ARS price for BTC
+     */
+    @Override
+    public Set<ExchangeRate> doGet() {
+        Set<ExchangeRate> result = new HashSet<>();
+        String key = "ARS";
+        Double rate = getARSBlueMarketData().averageBlueRate();
+        if (rate > 0.0d) {
+            result.add(new ExchangeRate(
+                    key,
+                    BigDecimal.valueOf(rate),
+                    new Date(),
+                    this.getName()
+            ));
+        }
+        return result;
+    }
+
+    private CryptoYaMarketData getARSBlueMarketData() {
+        return restTemplate.exchange(
+                RequestEntity
+                        .get(UriComponentsBuilder
+                                .fromUriString(CryptoYa.ARS_BTC_URL).build()
+                                .toUri())
+                        .build(),
+                new ParameterizedTypeReference<CryptoYaMarketData>() {
+                }
+        ).getBody();
+    }
+}
---
+++ src/main/java/bisq/price/util/cryptoya/CryptoYaMarketData.java
@@ -0,0 +1,75 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.price.util.cryptoya;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+@Getter
+@Setter
+public class CryptoYaMarketData {
+
+    private CryptoYaTicker argenbtc;
+    private CryptoYaTicker buenbit;
+    private CryptoYaTicker ripio;
+    private CryptoYaTicker ripioexchange;
+    private CryptoYaTicker satoshitango;
+    private CryptoYaTicker cryptomkt;
+    private CryptoYaTicker decrypto;
+    private CryptoYaTicker latamex;
+    private CryptoYaTicker bitso;
+    private CryptoYaTicker letsbit;
+    private CryptoYaTicker fiwind;
+    private CryptoYaTicker lemoncash;
+    private CryptoYaTicker bitmonedero;
+    private CryptoYaTicker belo;
+    private CryptoYaTicker tiendacrypto;
+    private CryptoYaTicker saldo;
+    private CryptoYaTicker kriptonmarket;
+    private CryptoYaTicker calypso;
+    private CryptoYaTicker bybit;
+    private CryptoYaTicker binance;
+
+    /**
+     *
+     * @return the avg ask price from all the exchanges that have updated ask prices (not older than 1 day)
+     *      if no market data available returns 0
+     */
+    public Double averageBlueRate() {
+        // filter more than 1 day old values with yesterday UTC timestamp
+        Long yesterdayTimestamp = Instant.now().minus(1, ChronoUnit.DAYS).getEpochSecond();
+        return streamLatestAvailableMarkets(yesterdayTimestamp).mapToDouble(CryptoYaTicker::getAsk)
+                .average()
+                .orElse(0.0d);
+    }
+
+    private Stream<CryptoYaTicker> streamLatestAvailableMarkets(Long startingTime) {
+        return Stream.of(argenbtc, buenbit, ripio, ripioexchange, satoshitango,
+                cryptomkt, decrypto, latamex, bitso, letsbit, fiwind,
+                lemoncash, bitmonedero, belo, tiendacrypto, saldo,
+                kriptonmarket, calypso, bybit, binance)
+                    .filter(Objects::nonNull)
+                    .filter(rate -> rate.getTime() > startingTime);
+    }
+}
