4c3e2359f533412acc895237872dee94c3a058dc
Boiling <alvasw@protonmail.com>
2023-09-06 13:04:04 +0200

    ExchangeRateProvider: Don't create List and throw it away

--- src/main/java/bisq/price/spot/ExchangeRateProvider.java
+++ src/main/java/bisq/price/spot/ExchangeRateProvider.java
@@ -240,7 +240,6 @@ public abstract class ExchangeRateProvider extends PriceProvider<Set<ExchangeRat
             List<Ticker> finalTickersRetrievedFromExchange = tickersRetrievedFromExchange;
             Stream.of(desiredFiatPairs, desiredCryptoPairs)
                     .flatMap(Collection::stream)
-                    .collect(Collectors.toList())
                     .forEach(cp -> {
                         try {
 
