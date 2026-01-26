5c0a05223469cea10f6e50639821e5ba4957aace
KimStrand <231482327+KimStrand@users.noreply.github.com>
2026-01-26 08:46:14 +0100

    Remove BGN due to EUR adoption in 2026

--- src/main/java/bisq/price/common/CurrencyUtil.java
+++ src/main/java/bisq/price/common/CurrencyUtil.java
@@ -41,20 +41,20 @@ public class CurrencyUtil {
     public static final Set<String> ALL_FIAT_CURRENCIES = Set.of(
             "AFN", "ALL", "DZD", "AOA", "ARS", "AMD", "AWG", "AUD", "AZN", "BSD",
             "BHD", "BDT", "BBD", "BYN", "BZD", "BMD", "BTN", "BOB", "BAM", "BWP",
-            "BRL", "GBP", "BND", "BGN", "BIF", "XPF", "KHR", "CAD", "CVE", "KYD",
-            "XAF", "CLP", "CNY", "COP", "KMF", "CDF", "CRC", "CUP", "CZK", "DKK",
-            "DJF", "DOP", "XCD", "EGP", "ERN", "ETB", "EUR", "FKP", "FJD", "GMD",
-            "GEL", "GHS", "GIP", "GTQ", "GNF", "GYD", "HTG", "HNL", "HKD", "HUF",
-            "ISK", "INR", "IDR", "IRR", "IQD", "ILS", "JMD", "JPY", "JOD", "KZT",
-            "KES", "KWD", "KGS", "LAK", "LBP", "LSL", "LRD", "LYD", "MOP", "MKD",
-            "MGA", "MWK", "MYR", "MVR", "MRU", "MUR", "MXN", "MDL", "MNT", "MAD",
-            "MZN", "MMK", "NAD", "NPR", "ANG", "TWD", "NZD", "NIO", "NGN", "KPW",
-            "NOK", "OMR", "PKR", "PAB", "PGK", "PYG", "PEN", "PHP", "PLN", "QAR",
-            "RON", "RUB", "RWF", "SVC", "WST", "SAR", "RSD", "SCR", "SLE", "SGD",
-            "SBD", "SOS", "ZAR", "KRW", "SSP", "LKR", "SHP", "SDG", "SRD", "SZL",
-            "SEK", "CHF", "SYP", "STN", "TJS", "TZS", "THB", "TOP", "TTD", "TND",
-            "TRY", "TMT", "USD", "UGX", "UAH", "AED", "UYU", "UZS", "VUV", "VES",
-            "VND", "XOF", "YER", "ZMW", "ZWL"
+            "BRL", "GBP", "BND", "BIF", "XPF", "KHR", "CAD", "CVE", "KYD", "XAF",
+            "CLP", "CNY", "COP", "KMF", "CDF", "CRC", "CUP", "CZK", "DKK", "DJF",
+            "DOP", "XCD", "EGP", "ERN", "ETB", "EUR", "FKP", "FJD", "GMD", "GEL",
+            "GHS", "GIP", "GTQ", "GNF", "GYD", "HTG", "HNL", "HKD", "HUF", "ISK",
+            "INR", "IDR", "IRR", "IQD", "ILS", "JMD", "JPY", "JOD", "KZT", "KES",
+            "KWD", "KGS", "LAK", "LBP", "LSL", "LRD", "LYD", "MOP", "MKD", "MGA",
+            "MWK", "MYR", "MVR", "MRU", "MUR", "MXN", "MDL", "MNT", "MAD", "MZN",
+            "MMK", "NAD", "NPR", "ANG", "TWD", "NZD", "NIO", "NGN", "KPW", "NOK",
+            "OMR", "PKR", "PAB", "PGK", "PYG", "PEN", "PHP", "PLN", "QAR", "RON",
+            "RUB", "RWF", "SVC", "WST", "SAR", "RSD", "SCR", "SLE", "SGD", "SBD",
+            "SOS", "ZAR", "KRW", "SSP", "LKR", "SHP", "SDG", "SRD", "SZL", "SEK",
+            "CHF", "SYP", "STN", "TJS", "TZS", "THB", "TOP", "TTD", "TND", "TRY",
+            "TMT", "USD", "UGX", "UAH", "AED", "UYU", "UZS", "VUV", "VES", "VND",
+            "XOF", "YER", "ZMW", "ZWL"
     );
 
     public static boolean isCryptoCurrency(String code) {
